2a5f739191aa740df49c258b891b8cf79ab07801
hanifulakbaro <hanifulakbaro@gmail.com>
2024-09-15 17:35:50 +0700

    new commit

--- board/__init__.py
+++ board/__init__.py
@@ -1,21 +1,9 @@
-import os
-from dotenv import load_dotenv
 from flask import Flask
-
-from board import pages, posts, database
-
-
-load_dotenv()
+from board import pages
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_prefixed_env()
-
-    database.init_app(app)
 
     app.register_blueprint(pages.bp)
-    app.register_blueprint(posts.bp)
-    print(f"Current Environment: {os.getenv('ENVIRONMENT')}")
-    print(f"Using Database: {app.config.get('DATABASE')}")
     return app
 
